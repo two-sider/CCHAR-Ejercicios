a9a0ae9ab371836f9b3cb625e7f8e275863c6647
two-sider <erickbeltranlopez@gmail.com>
2019-05-08 15:15:58 -0400

    Primer Git

---
+++ myApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad;
+            Console.WriteLine("Cual es tu edad");
+            edad = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Tu edad es {0}",edad);
+        }
+    }
+}
