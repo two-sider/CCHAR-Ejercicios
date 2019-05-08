cc3234318c877850645ebe2f93ea654914cd10f2
two-sider <erickbeltranlopez@gmail.com>
2019-05-08 15:36:47 -0400

    ejercicio comprobacion de edad

--- myApp/Program.cs
+++ myApp/Program.cs
@@ -7,9 +7,28 @@ namespace myApp
         static void Main(string[] args)
         {
             int edad;
+
+            while (true){
             Console.WriteLine("Cual es tu edad");
             edad = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Tu edad es {0}",edad);
+            
+            if (edad >=18)
+            {
+                Console.WriteLine("Tu edad es {0} y eres mayor de edad",edad);
+                break;
+            }
+            else if (edad < 0) 
+            {
+                Console.WriteLine("No existes, por favor intenta con una edad real");
+            }
+            else
+            {
+                Console.WriteLine("Tu edad es {0} y no eres mayor de edad",edad);
+                break;
+            }
+            }
+
+
         }
     }
 }
