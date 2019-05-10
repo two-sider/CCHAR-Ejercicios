0e7101d21f6341c88758ba87eaf3225a6a4cb429
two-sider <erickbeltranlopez@gmail.com>
2019-05-10 15:12:21 -0400

    ML actualizacion

---
+++ myMLApp/myMLApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using myMLAppML.Model.DataModels;
+using Microsoft.ML;
+
+namespace myMLApp{
+
+    class Program{
+
+        static void Main(string[] args){
+            ConsumeModel();
+        }
+
+        public static void ConsumeModel(){
+            // Load the model
+
+            MLContext mlContext = new MLContext();
+
+            ITransformer mlModel = mlContext.Model.Load("MLModel.zip", out var modelInputSchema);
+
+            var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
+
+            // Use the code below to add input data
+            var input = new ModelInput();
+            input.SentimentText = "Type your sentiment";
+
+            // Try model on sample data
+            ModelOutput result = predEngine.Predict(input);
+
+            Console.WriteLine("Result=" + result.Prediction);
+        }
+    }
+}
